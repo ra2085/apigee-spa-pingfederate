4786f5edd1a00384bc73211d1cb9a05448ba99f3
Ruben Gonzalez <gonzalezruben@ad.corp.google.com>
2018-10-26 16:12:48 -0500

    add activity button

--- src/app/authorization.service.ts
+++ src/app/authorization.service.ts
@@ -170,13 +170,11 @@ export class AuthorizationService {
               method: 'GET',
               dataType: 'json',
               headers: {'Authorization': `Bearer ${accessToken}`}
-            }).then((userinfo, err) => {
-			  if (err) {
-				  authorize();
-			  } else {
-			      this._userInfos.next(userinfo);
-			  }
-         });
+            }).then((userinfo) => {
+			  this._userInfos.next(userinfo);
+         }).catch((err) => {
+			 authorize();
+		 });
       }
   }
 
