f46dc384ec48d8ad3062efc8c60787faa76c286e
Ruben Gonzalez <gonzalezruben@ad.corp.google.com>
2018-11-11 19:51:11 -0600

    role migration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,8 @@ import { AuthorizationConfig, GeneralEnvironmentInfo } from '../app/authorizatio
 
 export const environment: AuthorizationConfig & GeneralEnvironmentInfo  = {
   production: false,
-  issuer_uri: 'https://35.237.173.209:9031',
+//  issuer_uri: 'https://35.237.173.209:9031',
+  issuer_uri: 'https://gonzalezruben-eval-test.apigee.net/ping_apigee_oauth',
   client_id: 'client_a',
   redirect_uri: 'https://4200-dot-4427029-dot-devshell.appspot.com/callback',
   extras: {
